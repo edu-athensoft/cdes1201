b1b80fad2e67e00d30eb233b1287266f1697da25
cindyliu <cindyliu551@gmail.com>
2019-09-18 12:28:09 -0400

    submitted file on mean

--- session01/statistics/mean.py
+++ session01/statistics/mean.py
@@ -1,12 +1,11 @@
-numstr = input("input a set of numbers separated by space: ")
-
-
+numstr = input("to find mean, input a set of numbers separated by space: ")
 try:
     numlist = numstr.split( )
     numint = map(int, numlist)
     numint = list(map(int, numlist))#conv to int
-    numint.sort()#min to max
-    print(numint[len(numint)-1])
+    import math
+    print(math.fsum(numint[:]) / len(numint))
+
 except ValueError:#not num or separated by comma
     print("please enter valid numbers and separate them by space")
 except IndexError:#no input
