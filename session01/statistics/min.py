c3f48f9dfff3b5c0814b7030b99ccdc363ca65d4
cindyliu <cindyliu551@gmail.com>
2019-09-18 11:29:25 -0400

    submitted file on min

---
+++ session01/statistics/min.py
@@ -0,0 +1,12 @@
+numstr = input("input a set of numbers separated by space: ")
+try:
+    numlist = numstr.split( )
+    numint = map(int, numlist)
+    numint = list(map(int, numlist))#conv to int
+    numint[0]
+    numint.sort()#min to max
+    print(numint[0])
+except ValueError:#not num or separated by comma
+    print("please enter valid numbers and separate them by space")
+except IndexError:#no input
+    print("please enter at least one number")
