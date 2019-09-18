fd97f8b5cbaa52db11765e4b4a971b28bb136af7
cindyliu <cindyliu551@gmail.com>
2019-09-18 11:35:40 -0400

    submitted file on min

--- session01/statistics/min.py
+++ session01/statistics/min.py
@@ -1,9 +1,8 @@
-numstr = input("input a set of numbers separated by space: ")
+numstr = input("to find min, input a set of numbers separated by space: ")
 try:
     numlist = numstr.split( )
     numint = map(int, numlist)
     numint = list(map(int, numlist))#conv to int
-    numint[0]
     numint.sort()#min to max
     print(numint[0])
 except ValueError:#not num or separated by comma
