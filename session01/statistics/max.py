2f95409e911dfeb96ef1b2b4b292a508d9762ce8
cindyliu <cindyliu551@gmail.com>
2019-09-18 11:36:07 -0400

    submitted file on max

--- session01/statistics/max.py
+++ session01/statistics/max.py
@@ -1,9 +1,8 @@
-numstr = input("input a set of numbers separated by space: ")
+numstr = input("to find max, input a set of numbers separated by space: ")
 try:
     numlist = numstr.split( )
     numint = map(int, numlist)
     numint = list(map(int, numlist))#conv to int
-    numint[0]
     numint.sort()#min to max
     print(numint[len(numint)-1])
 except ValueError:#not num or separated by comma
