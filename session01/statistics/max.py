3bd2babf53394659f31d6a7949decca8f1e60b40
cindyliu <cindyliu551@gmail.com>
2019-09-24 19:05:06 -0400

    submitted file

--- session01/statistics/max.py
+++ session01/statistics/max.py
@@ -3,8 +3,9 @@ try:
     numlist = numstr.split( )
     numint = map(float, numlist)
     numint = list(map(float, numlist))#conv to int
-    numint.sort()#min to max
-    print(numint[len(numint)-1])
+    #numint.sort()#min to max
+    #print(numint[len(numint)-1])
+    print(max(numint))
 except ValueError:#not num or separated by comma
     print("please enter valid numbers and separate them by space")
 except IndexError:#no input
--- session01/statistics/min.py
+++ session01/statistics/min.py
@@ -1,10 +1,11 @@
 numstr = input("to find min, input a set of numbers separated by space: ")
 try:
-    numlist = numstr.split( )
+    numlist = numstr.split()
     numint = map(float, numlist)
-    numint = list(map(float, numlist))#conv to int
-    numint.sort()#min to max
-    print(numint[0])
+    numint = list(numint)#conv to int
+    #numint.sort()#min to max
+    #print(numint[0])
+    print(min(numint))
 except ValueError:#not num or separated by comma
     print("please enter valid numbers and separate them by space")
 except IndexError:#no input
