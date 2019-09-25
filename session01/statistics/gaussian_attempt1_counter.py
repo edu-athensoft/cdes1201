a419b09269924255315dee2949049997ee28210c
cindyliu <cindyliu551@gmail.com>
2019-09-25 14:53:18 -0400

    sketch file on gaussian

--- session01/statistics/gaussian_attempt1_counter.py
+++ session01/statistics/gaussian_attempt1_counter.py
@@ -1,9 +1,10 @@
 from collections import Counter
+import math
 try:
 
     datastr = input('please enter data set')
     datalist = datastr.split()
-    dataint = list(map(int, datalist))
+    dataint = list(map(float, datalist))
     low = int(input("please enter lower boundary"))
     high = int(input("please enter higher boundary"))
     bins = int(input("please enter number of intervals"))
@@ -17,5 +18,13 @@ try:
 
 
     print(histogram(dataint, low, high, bins))
+    i = 0
+    datafre = []
+    while i < len(dataint):
+        #math.fsum(dataint[:])
+        datafre.append(dataint[i] / math.fsum(dataint[:]))
+        i += 1
+    print(datafre)
+
 except:
     print("please try again")
