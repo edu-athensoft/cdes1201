7eefebdcee7ef25cbb40b26f452adb771221d1cd
cindyliu <cindyliu551@gmail.com>
2019-09-26 23:12:29 -0400

    hard code file on var(sketch)

---
+++ session01/statistics/var2.py
@@ -0,0 +1,20 @@
+places = []
+import math
+
+with open(input('please enter txt file name'), 'r') as filehandle:
+    for line in filehandle:
+        currentPlace = line[:]
+
+        # add item to the list
+        places.append(currentPlace)
+print(places)
+numint = list(map(float, places))  # conv to int
+mean = math.fsum(numint[:]) / len(places)
+diff = []
+i = 0
+while i < len(places):
+    square = (mean - places[i]) ** 2
+    i += 1
+    diff.append(square)
+    var = math.fsum(diff[:])
+print(var)
