3aea30e020334a8bf9c83f048184998ccfa43fee
cindyliu <cindyliu551@gmail.com>
2019-09-26 20:47:06 -0400

    sketch file on gaussian

--- session01/statistics/gaussian_attempt1_counter.py
+++ session01/statistics/gaussian_attempt1_counter.py
@@ -1,30 +1,30 @@
 from collections import Counter
 import math
+
 try:
 
     datastr = input('please enter data set')
     datalist = datastr.split()
     dataint = list(map(float, datalist))
-    low = int(input("please enter lower boundary"))
-    high = int(input("please enter higher boundary"))
+    low = float(input("please enter lower boundary"))
+    high = float(input("please enter higher boundary"))
     bins = int(input("please enter number of intervals"))
 
 
     def histogram(iterable, low, high, bins):
 
-        step = (high - low + 0.0) / bins
-        dist = Counter((float(x) - low) // step for x in iterable)
-        return [dist[b] for b in range(bins)]
-
+        step = (high - low + 0.0) / bins  # length of interval
+        dist = Counter((float(x) - low) // step for x in iterable)  # return list for number of data in each interval
+       # return [dist[b] for b in range(bins)]
+        return [dist[b] / math.fsum(dist[s] for s in range(bins)) for b in range(bins)]
 
     print(histogram(dataint, low, high, bins))
-    i = 0
-    datafre = []
-    while i < len(dataint):
-        #math.fsum(dataint[:])
-        datafre.append(dataint[i] / math.fsum(dataint[:]))
-        i += 1
-    print(datafre)
+   # i = 0
+   # datafre = []
+   # while i < len(dataint):
+       # datafre.append(dataint[i] / math.fsum(dataint[:]))
+       # i += 1
+   # print(datafre)
 
 except:
     print("please try again")
